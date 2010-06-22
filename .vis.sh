29c84aadc3ee74f40c9d904e01a5846072faa952
XPac <xpac27@gmail.com>
2010-06-22 12:45:27 +0200

    Init repository

---
+++ .vis.sh
@@ -0,0 +1,29 @@
+################################
+#
+# VIS
+#
+# authors : Nyvath, YTG, VCogne
+# date    : 06/22/10
+#
+################################
+
+# Open vim in a session related to this repository's current branch
+function vis
+{
+    if [[ "$1" = "" ]]; then
+        if [[ "`ls ~/.vim/sessions/ | grep $branch`" = "" ]]; then
+            vim -c 'SessionSaveAs '`git_session_name`
+        else
+            vim -c 'SessionOpen '`git_session_name`
+        fi
+    else
+        git co $1  && vim -c 'SessionOpen '$1
+   fi
+}
+
+# Get a proper session name for vim
+git_session_name()
+{
+   echo "$(pwd | sed 's/\//_/g')_$(git branch | grep \* | cut -f 2 -d ' ')"
+}
+
